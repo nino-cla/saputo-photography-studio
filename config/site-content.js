96729eb0f346ea1e5f667b921ca758dfd5d52b3f
skymontanop <pamontan@emeal.nttdata.com>
2026-05-11 14:22:47 +0200

    feat: new mail address

--- config/site-content.js
+++ config/site-content.js
@@ -1,6 +1,6 @@
 const siteContent = {
   tab_title: "Nino Claudio Saputo | Fotografo Professionista a Palermo",
-  email: "sapunino08@gmail.com",
+  email: "info@ninoclaudiofotografo.it",
   phone: "+39 389 426 2969",
   whatsapp: "393894262969",
   instagram: "https://www.instagram.com/ninocla__fotografo_palermo?igsh=MWE2cm9oYW9naHk1Zw=="
